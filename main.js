7ddacd612545d4305ed54482cec20adf72919c3f
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-04-09 18:34:46 -0400

    close now

--- main.js
+++ main.js
@@ -9,66 +9,59 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
-var name = "";
-var destination = "";
-var firstTime ='';
-var freq = 0;
 var now = moment().format('HH:mm');
 
 //console.log(moment(firstTime).add(freq,'m').format("hh:mm"));
-console.log(now);   
-//console.log(firstTime);
+console.log(now);
 
-//moment().format('hh:mm');
 
 $("#add-train").on("click", function () {
-    // Don't refresh the page!
+    var name = $("#name-input").val().trim();
+    var des = $("#destination-input").val().trim();
+    console.log(`${name} ${des}`);
+    var firstTime = $("#firstTrain-input").val().trim();
+    var freq = $("#freq-input").val().trim();
     event.preventDefault();
+    if (name == "" || des == "" || freq == 0 || firstTime == 0 ) {
+        alert("Validate all inputs");
+    } else {
+        var firstCon = moment(firstTime, 'HH:mm').subtract(1, 'years');
+        console.log(firstCon);
+        var diff = moment().diff(firstCon, 'm');
+        var remain = diff % freq;
+        var minLeft = freq - remain;
+        var next = moment().add(minLeft, 'm').format('hh:mm A');
+        database.ref().push({
+            name: name,
+            des: des,
+            firstTime: firstTime,
+            diff: diff,
+            minLeft: minLeft,
+            freq: freq,
+            next: next
+        });
+    }
 
-    
-    name = $("#name-input").val().trim();
-    des = $("#destination-input").val().trim();
-    firstTime = $("#firstTrain-input").val().trim();
-    freq = $("#freq-input").val().trim();
-   
-   var firstCon = moment(firstTime, 'HH:mm').subtract(1, 'years');
-   console.log(firstCon);
-    var diff = moment().diff(firstCon, 'm');
-    var remain = diff % freq;
-    var minLeft = freq - remain;
-   
-   var next = moment().add(minLeft,'m').format('hh:mm A');
-   
-   
-    database.ref().push({
-        name: name,
-        des: des,
-        firstTime: firstTime,
-         diff : diff,
-         minLeft : minLeft,
-         freq: freq,
-         next: next
-    });
+
+
 
 });
 
 
 //database.ref().on("value", function (snapshot) {
-    database.ref().on("child_added", function (childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
     // Log everything that's coming out of snapshot
-   // console.log(snapshot.val());
+    // console.log(snapshot.val());
     console.log(childSnapshot.val().name);
     console.log(childSnapshot.val().des);
     console.log(childSnapshot.val().firstTime);
     console.log(childSnapshot.val().freq);
     console.log(childSnapshot.val().next);
 
-        $("#row").append("<div id='well'><span class='nameDis'> " + childSnapshot.val().name+
-        "</span><span class='desDis'> " + childSnapshot.val().des+
+    $("#row").append("<div id='well'><span class='nameDis'> " + childSnapshot.val().name +
+        "</span><span id='desDis'> " + childSnapshot.val().des +
         "</span><span class='freq'> " + childSnapshot.val().freq + " </span></div>")
-        
-    }, function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-    });
-    
+
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
